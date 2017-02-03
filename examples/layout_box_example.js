6e24e2ece623caeba8225c8892bb6407218943a7
Christian Bjartli <git@c.bjart.li>
2017-02-03 15:57:48 +0100

    Added layout section

--- examples/layout_box_example.js
+++ examples/layout_box_example.js
@@ -16,19 +16,19 @@ const MyWindow = new Lang.Class({
         this.add(this.box);
 
         this.button1 = new Gtk.Button({label: "Hello"});
-        this.button1.connect("clicked", this.on_button1_clicked);
+        this.button1.connect("clicked", this.onButton1Clicked);
         this.box.pack_start(this.button1, true, true, 0);
 
         this.button2 = new Gtk.Button({label: "Goodbye"});
-        this.button2.connect("clicked", this.on_button2_clicked);
+        this.button2.connect("clicked", this.onButton2Clicked);
         this.box.pack_start(this.button2, true, true, 0);
     },
 
-    on_button1_clicked: function(widget) {
+    onButton1Clicked: function(widget) {
         print("Hello")
     },
 
-    on_button2_clicked: function(widget) {
+    onButton2Clicked: function(widget) {
         print("Goodbye")
     }
 });
